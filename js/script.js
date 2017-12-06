16db63c3cf915719a593f4815d006091d27afda1
Hauke Grothues <hauke.grothues@web.de>
2017-12-06 16:26:06 +0100

    undo dumb mistakes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
- var toAppend = "";
+var toAppend = "";
 $(document).ready(function() {
-  var names = ["aDrive", "ESL_SC2"];
+  var names = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
   for (var i = 0; i < names.length; i++) {
     APIrequest(names[i]);
   }
@@ -14,11 +14,11 @@ request.open('GET', "https://api.twitch.tv/kraken/streams/" + name + "?client_id
 request.onload = function() {
   if (this.status >= 200 && this.status < 400) {
     // Success!
-    var data = JSON.parse(this.response);
-    toAppend += "<div class='contaner'> <div class='card'> <div class='card-body'> <img class='profil-pic' src='" + data.stream.channel.logo + "' alt ='channel logo'> <h4 class='card-title'>" + name + "</h4> <br /> <p class='card-text'>" + data.stream.channel.status + "</p> <a class='btn btn-live' href='" + data.stream.channel.url + "' role='button'> LIVE </a> </div> </div> </div>";
-    $(".results").html(toAppend);
+        var data = JSON.parse(this.response);
+      toAppend += "<div class='container'> <div class='card'> <div class='card-body'> <img class='profil-pic' src='" + data.stream.channel.logo + "' alt ='channel logo'> <h4 class='card-title'>" + name + "</h4> <br /> <p class='card-text'>" + data.stream.channel.status + "</p> <a class='btn btn-live' href='" + data.stream.channel.url + "' role='button'> LIVE </a> </div> </div> </div>";
+      $(".results").html(toAppend);
   }
-
+  
   else {
     // We reached our target server, but it returned an error
     alert("Ndh");
