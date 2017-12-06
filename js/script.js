229eba914932fed8d6a1cc61e198a463178052a9
Hauke Grothues <hauke.grothues@web.de>
2017-12-06 16:41:33 +0100

    fixes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var toAppend = "";
 $(document).ready(function() {
-  var names = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  var names = ["aDrive", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
   for (var i = 0; i < names.length; i++) {
     APIrequest(names[i]);
   }
@@ -13,12 +13,13 @@ request.open('GET', "https://api.twitch.tv/kraken/streams/" + name + "?client_id
 
 request.onload = function() {
   if (this.status >= 200 && this.status < 400) {
-    // Success!
-        var data = JSON.parse(this.response);
+    var data = JSON.parse(this.response);
+    if (data.stream == null) {
+
+    }
       toAppend += "<div class='container'> <div class='card'> <div class='card-body'> <img class='profil-pic' src='" + data.stream.channel.logo + "' alt ='channel logo'> <h4 class='card-title'>" + name + "</h4> <br /> <p class='card-text'>" + data.stream.channel.status + "</p> <a class='btn btn-live' href='" + data.stream.channel.url + "' role='button'> LIVE </a> </div> </div> </div>";
       $(".results").html(toAppend);
   }
-  
   else {
     // We reached our target server, but it returned an error
     alert("Ndh");
