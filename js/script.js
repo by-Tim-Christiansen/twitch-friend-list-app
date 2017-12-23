698de50e8052a8ccd44fcf229794766ae345975a
Hauke Grothues <hauke.grothues@gmail.com>
2017-12-23 13:01:24 +0100

    I fucking did it
    A new update of the JS which shows whether the channels are online or offline.

--- js/script.js
+++ js/script.js
@@ -1,35 +1,50 @@
-var toAppend = "";
 $(document).ready(function() {
-  var names = ["aDrive", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  var toAppend = "";
+  var names = ["aDrive", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "mckyTV", "ungespielt"];
+  
+  function callIfOffline(name){
+    var request2 = new XMLHttpRequest();
+    request2.open('GET', "https://api.twitch.tv/kraken/channels/" + name + "?client_id=syzby9glp3k5myd4tvro3mg5a5pruj", true);
+    request2.onload = function() {
+      if (this.status >= 200 && this.status < 400) {
+        var data2 = JSON.parse(this.response);
+        toAppend += "<div class='container'> <div class='card'> <div class='card-body'> <img class='profil-pic' src='" + data2.logo + "' alt ='channel logo'> <h4 class='card-title'>" + name + "</h4> <br /> <p class='card-text'>" + data2.status + "</p> <a class='btn btn-off' href='" + data2.url + "' role='button'> OFFLINE </a> </div> </div> </div>";
+        $(".results").html(toAppend);
+      }
+      else {
+        alert("Oops! Something went wrong, please try again.");
+      }
+    };
+    request2.onerror = function() {
+      alert("Oops! Something went wrong, please try again.");
+    };
+    request2.send();
+  }
+  
+  function APIrequest(name) {
+    var request = new XMLHttpRequest();
+    request.open('GET', "https://api.twitch.tv/kraken/streams/" + name + "?client_id=syzby9glp3k5myd4tvro3mg5a5pruj", true);
+    request.onload = function() {
+      if (this.status >= 200 && this.status < 400) {
+        var data = JSON.parse(this.response);
+        if (data.stream !== null) {
+          toAppend += "<div class='container'> <div class='card'> <div class='card-body'> <img class='profil-pic' src='" + data.stream.channel.logo + "' alt ='channel logo'> <h4 class='card-title'>" + name + "</h4> <br /> <p class='card-text'>" + data.stream.channel.status + "</p> <a class='btn btn-live' href='" + data.stream.channel.url + "' role='button'> LIVE </a> </div> </div> </div>"; 
+          $(".results").html(toAppend);
+        }
+        else {
+          callIfOffline(name);
+        }
+      }
+      else {
+        alert("Ndh");
+      }
+    };
+    request.onerror = function() {
+      alert("nf");
+    };
+    request.send();
+  }
   for (var i = 0; i < names.length; i++) {
     APIrequest(names[i]);
   }
 });
-
-function APIrequest(name) {
-var request = new XMLHttpRequest();
-
-request.open('GET', "https://api.twitch.tv/kraken/streams/" + name + "?client_id=syzby9glp3k5myd4tvro3mg5a5pruj", true);
-
-request.onload = function() {
-  if (this.status >= 200 && this.status < 400) {
-    var data = JSON.parse(this.response);
-    if (data.stream == null) {
-
-    }
-      toAppend += "<div class='container'> <div class='card'> <div class='card-body'> <img class='profil-pic' src='" + data.stream.channel.logo + "' alt ='channel logo'> <h4 class='card-title'>" + name + "</h4> <br /> <p class='card-text'>" + data.stream.channel.status + "</p> <a class='btn btn-live' href='" + data.stream.channel.url + "' role='button'> LIVE </a> </div> </div> </div>";
-      $(".results").html(toAppend);
-  }
-  else {
-    // We reached our target server, but it returned an error
-    alert("Ndh");
-  }
-};
-
-request.onerror = function() {
-  // There was a connection error of some sort
-  alert("nf");
-};
-
-request.send();
-}
